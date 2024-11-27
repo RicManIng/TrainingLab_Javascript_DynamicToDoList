d54ee778d1596f92c4050ee259ec42292fb0699b
Riccardo Mancinelli <riccardo.mancinelliing@gmail.com>
2024-11-27 07:36:14 +0100

    - create website cover page

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once 'head.php'; ?>
+    <title>Dynamic ToDo List</title>
+    <link rel="stylesheet" href="resources/css/index.min.css">
+</head>
+<body>
+    <main>
+        <h1>Welcome to your family management system</h1>
+        <a href="login.php?state=login">Start Now</a>
+    </main>
+</body>
+</html>
